3c3f56f427767fe1e58c4d45960b6acc23524d07
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2021-10-25 09:20:33 +0100

    match dwm black with .bg  black

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=10" };
 static const char dmenufont[]       = "Fira Code:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#070b14";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
