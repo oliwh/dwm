71b941c5bb3c8ae69a95d6eddfc82c359970e90f
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-01-31 22:21:35 +0000

    applied fullgaps patch

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -98,6 +99,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ NULL,                         XK_Print,  spawn,          {.v = flameshotcmd } },
 	{ ControlMask,                  XK_Print,  spawn,          {.v = flameshotdelaycmd } },
 	{ MODKEY,                       XK_Tab,    shiftview,      { .i = +1} },
