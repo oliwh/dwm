486bc06cb5c10f9a92587c8f34f3bea51dd57dbe
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-02-01 07:47:23 +0000

    added default gap size keybind

--- config.def.h
+++ config.def.h
@@ -102,6 +102,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = 15  } },
 	{ NULL,                         XK_Print,  spawn,          {.v = flameshotcmd } },
 	{ ControlMask,                  XK_Print,  spawn,          {.v = flameshotdelaycmd } },
 	{ MODKEY,                       XK_Tab,    shiftview,      { .i = +1} },
